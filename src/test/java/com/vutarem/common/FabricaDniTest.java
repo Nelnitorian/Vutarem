24a6e105c5f31b35185ddc94885dd22ea85d8751
Nelnitorian <jujoroal@gmail.com>
2023-04-10 11:16:33 +0200

    Mejorados tests

--- src/test/java/com/vutarem/common/FabricaDniTest.java
+++ src/test/java/com/vutarem/common/FabricaDniTest.java
@@ -6,8 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FabricaDniTest {
 
     @Test
-    public void testFabricaDni() {
-        assertTrue(FabricaDni.creaDni("12345678A") != null);        
+    public void testDniCorrecto() {
+        assertNotNull(FabricaDni.creaDni("12345678A"));        
+    }
+
+
+    @Test
+    public void testDniIncorrectoLetras() {
+        assertNull(FabricaDni.creaDni("ABCDEFGHI"));        
+    }
+
+    @Test
+    public void testDniIncorrectoTamanio() {
+        assertNull(FabricaDni.creaDni("123456789A"));        
     }
 
 }
